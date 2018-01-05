098d4a61096f161721710e936e6d99496a1879a3
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-05 17:05:47 +0800

    add spec about #5, #6, #7

--- lib/corpus.rb
+++ lib/corpus.rb
@@ -2,13 +2,25 @@ module Corpus
   require 'youtube-dl.rb'
 
   def get_corpus(url)
-    data = download_data(url)
-    log_data(data)
+    # data = download_data(url)
+    # log_data(data)
+    # '執行完成'
   end
 
   def download_data(url)
+    # 下載 YoutubeDL.download url, options
+    # '掠影片成功'
   end
 
   def log_data(data)
+    # '存檔成功'
+  end
+
+  def has_cc(data)
+    # '字幕掠成功'
+  end
+
+  def has_audio(data)
+    # '字幕掠成功'
   end
 end
--- spec/lib/corpus_spec.rb
+++ spec/lib/corpus_spec.rb
@@ -6,38 +6,53 @@ RSpec.describe Corpus do
     extend Corpus
   end
 
-  context '有掠corpus' do
+  context '有掠corpus動作' do
     it do
-        下載 = spy('corpus')
-        下載.download_data(url)
-        下載.to have_received(:download).with(url)
+        下載 = spy('youtube-dl')
+        存檔 = spy('log_data')
+        allow(下載).to receive(:message){'掠corpus動作完成'}
+        allow(存檔).to receive(:message){'存corpus動作完成'}
+        expect(get_corpus(url)).to eq '執行完成'
     end
   end
 
-  context '掠corpus' do
+  context '掠影片' do
     it '掠有' do
-      下載 = spy('corpus')
-      allow(下載).to receive(:download_data).with(url).and_return('掠影片成功')
-      expect(下載.download_data(url)).to eq '掠影片成功'
+      下載 = spy('youtube-dl')
+      allow(下載).to receive(:message){'掠有影片'}
+      expect(download_data(url)).to eq '掠影片成功'
     end
 
     it '掠無' do
-      下載=spy('corpus')
-      allow(下載).to receive(:download_data).with(url).and_raise('掠影片失敗')
-      expect{下載.download_data(url)}.to raise_error '掠影片失敗'
+      下載 = spy('youtube-dl')
+      allow(下載).to receive(:message){'掠無影片'}
+      expect{download_data(url)}.to raise_error '掠影片失敗'
     end
   end
 
   context '掠字幕' do
     it '掠有' do
-      下載=spy('corpus')
-      allow(下載).to receive(:has_cc).and_return('字幕掠有')
-      expect(下載.has_cc).to eq '字幕掠有'
+      下載字幕 = spy('youtube-dl-cc')
+      allow(下載字幕).to receive(:message){'字幕掠有'}
+      expect(has_cc(下載字幕)).to eq '字幕掠成功'
     end
     it '掠無' do
-      下載=spy('corpus')
-      allow(下載).to receive(:has_cc).and_return('字幕掠無')
-      expect(下載.has_cc).to eq '字幕掠無'
+      下載字幕 = spy('youtube-dl-cc')
+      allow(下載字幕).to receive(:message){'字幕掠無'}
+      expect(has_cc(下載字幕)).to eq '字幕掠失敗'
+    end
+  end
+
+  context '掠聲音' do
+    it '掠有' do
+      下載聲音 = spy('youtube-dl-audio')
+      allow(下載聲音).to receive(:message){'聲音掠有'}
+      expect(has_audio(下載聲音)).to eq '聲音掠成功'
+    end
+    it '掠無' do
+      下載聲音 = spy('youtube-dl-audio')
+      allow(下載聲音).to receive(:message){'聲音掠無'}
+      expect(has_audio(下載聲音)).to eq '聲音掠失敗'
     end
   end
 end
