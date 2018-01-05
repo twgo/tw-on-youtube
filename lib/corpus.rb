8630020148213b4fdc21f537de47274e9438ef0e
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-05 15:22:59 +0800

    use rspec spy to mock function

--- lib/corpus.rb
+++ lib/corpus.rb
@@ -2,42 +2,13 @@ module Corpus
   require 'youtube-dl.rb'
 
   def get_corpus(url)
-    result='執行結果'
-    srt=has_srt
-    log_data(download_data(url, result), srt)
-    'finished'
+    data = download_data(url)
+    log_data(data)
   end
 
-  def download_data(url, result)
-    tries = 3
-    begin
-      if (result=='多一個檔案')
-        'success'
-      elsif result == '重試失敗'
-        retry_fail
-      else
-        'error'
-      end
-    rescue
-      tries -= 1
-      if tries > 0
-        retry
-      else
-        'retry-error'
-      end
-    end
+  def download_data(url)
   end
 
-  def log_data(data, srt)
-    # Download.create();
-    if (srt == '多一個字幕檔')
-      'logged with subtitle'
-    else
-      'logged'
-    end
-  end
-
-  def has_srt
-    '有字幕'
+  def log_data(data)
   end
 end
--- spec/lib/corpus_spec.rb
+++ spec/lib/corpus_spec.rb
@@ -6,27 +6,38 @@ RSpec.describe Corpus do
     extend Corpus
   end
 
-  context 'Can run all works to get corpus' do
-    it { expect(get_corpus(url)).to eq 'finished'  }
+  context '有掠corpus' do
+    it do
+        下載 = spy('corpus')
+        下載.download_data(url)
+        下載.to have_received(:download).with(url)
+    end
   end
 
-  context '掠有corpus' do
-    it { expect(download_data(url, '多一個檔案')).to eq 'success'   }
+  context '掠corpus' do
+    it '掠有' do
+      下載 = spy('corpus')
+      allow(下載).to receive(:download_data).with(url).and_return('掠影片成功')
+      expect(下載.download_data(url)).to eq '掠影片成功'
+    end
+
+    it '掠無' do
+      下載=spy('corpus')
+      allow(下載).to receive(:download_data).with(url).and_raise('掠影片失敗')
+      expect{下載.download_data(url)}.to raise_error '掠影片失敗'
+    end
   end
 
-  context '掠無corpus' do
-    it { expect(download_data(url, '沒多一個檔案')).to eq 'error'   }
-  end
-
-  context '掠無corpus Server Error' do
-    it { expect(download_data(url, '重試失敗')).to eq 'retry-error'   }
-  end
-
-  context '掠有字幕' do
-    it { expect(log_data('下載結果', '多一個字幕檔')).to eq 'logged with subtitle'   }
-  end
-
-  context '掠無字幕' do
-    it { expect(log_data('下載結果', '沒多一個字幕檔')).to eq 'logged'   }
+  context '掠字幕' do
+    it '掠有' do
+      下載=spy('corpus')
+      allow(下載).to receive(:has_cc).and_return('字幕掠有')
+      expect(下載.has_cc).to eq '字幕掠有'
+    end
+    it '掠無' do
+      下載=spy('corpus')
+      allow(下載).to receive(:has_cc).and_return('字幕掠無')
+      expect(下載.has_cc).to eq '字幕掠無'
+    end
   end
 end
