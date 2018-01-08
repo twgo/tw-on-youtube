7c55d99c00caba692828231d1a70b0e45c958cbe
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-08 09:45:40 +0800

    使用let重構測試主角

--- spec/lib/corpus_spec.rb
+++ spec/lib/corpus_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Corpus do
   end
 
   context '有掠corpus動作' do
+    let(:載存影片){double('get_corpus', url: :url)}
+
     it do
-        載存影片 = double('get_corpus', url: :url)
         allow(載存影片).to receive(:載影片){'掠corpus動作完成'}
         allow(載存影片).to receive(:存記錄){'存corpus動作完成'}
         expect(載存影片.載影片).to eq '掠corpus動作完成'
@@ -17,15 +18,14 @@ RSpec.describe Corpus do
   end
 
   context '載影片' do
+    let(:下載影片) { double('download_data', url: :url) }
+
     it '掠有' do
-      下載影片 = double('download_data', url: :url)
       allow(下載影片).to receive(:水管下載){'掠有影片'}
       expect(下載影片.水管下載).to eq '掠有影片'
     end
 
     it '掠無' do
-      下載影片 = double('download_data', url: :url)
-
       allow(下載影片).to receive(:水管下載){'掠無影片'}
       expect(下載影片.水管下載).to eq '掠無影片'
     end
