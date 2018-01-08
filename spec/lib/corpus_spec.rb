c7eaf96c93595dff00b624df3879bb0bb10cc2d8
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-08 10:47:30 +0800

    mock download

--- spec/lib/corpus_spec.rb
+++ spec/lib/corpus_spec.rb
@@ -15,12 +15,14 @@ RSpec.describe Corpus do
 
   context '載影片進資料庫' do
     it '成功' do
+      allow(載存影片).to receive(:載影片)
       expect(成功記錄).to eq 0
       載存影片
       expect(成功記錄).to eq 1
     end
 
     it '失敗' do
+      allow(載存影片).to receive(:載影片)
       expect(失敗記錄).to eq 0
       載存影片
       expect(失敗記錄).to eq 1
