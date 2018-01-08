b08e757d4d55b170f6a65d3c03817bf0a783320a
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-08 10:45:11 +0800

    fix spec with spy

--- spec/lib/corpus_spec.rb
+++ spec/lib/corpus_spec.rb
@@ -1,36 +1,29 @@
 require 'corpus'
 RSpec.describe Corpus do
-  let(:url) { 'https://www.youtube.com/watch?v=Oyio4UKLjLA' }
-
-  before(:each) do
-    extend Corpus
-  end
+  let(:載存影片){spy('get_corpus')}
+  let(:載影片){spy('download_data')}
+  let(:成功記錄){ 0 }
+  let(:失敗記錄){ 0 }
 
   context '有掠corpus動作' do
-    let(:載存影片){double('get_corpus', url: :url)}
-
-    it '有載影片' do
-        allow(載存影片).to receive(:載影片){'掠corpus動作完成'}
-        expect(載存影片.載影片).to eq '掠corpus動作完成'
-    end
-
-    it '有存影片' do
-        allow(載存影片).to receive(:存記錄){'存corpus動作完成'}
-        expect(載存影片.存記錄).to eq '存corpus動作完成'
+    it '有跑載影片一次' do
+      allow(載存影片).to receive(:載影片)
+      載存影片
+      expect(載存影片).to have_received(:載影片).once
     end
   end
 
-  context '載影片' do
-    let(:下載影片) { double('download_data', url: :url) }
-
-    it '掠有' do
-      allow(下載影片).to receive(:水管下載){'掠有影片'}
-      expect(下載影片.水管下載).to eq '掠有影片'
+  context '載影片進資料庫' do
+    it '成功' do
+      expect(成功記錄).to eq 0
+      載存影片
+      expect(成功記錄).to eq 1
     end
 
-    it '掠無' do
-      allow(下載影片).to receive(:水管下載){'掠無影片'}
-      expect(下載影片.水管下載).to eq '掠無影片'
+    it '失敗' do
+      expect(失敗記錄).to eq 0
+      載存影片
+      expect(失敗記錄).to eq 1
     end
   end
 end
