1cc8a37c1eaa3e520c444b0ab3e623f6fc18ef2e
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-09 17:03:53 +0800

    move data to folder named by uploader and format

--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -1,6 +1,9 @@
+require 'fileutils'
+
 class DownloadWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
+  DOCS_PATH = File.join(Rails.root, "public")
 
   def perform(*args)
     # Do something
@@ -10,11 +13,26 @@ class DownloadWorker
 
   def get_corpus(url)
     data = download_data(url)
+    data_format = data.filename.split('.').last
     log_data(data)
+
+    dirname = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader}")
+    unless File.directory?(dirname)
+      FileUtils.mkdir_p(dirname)
+    end
+    uploadername = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader}/#{data.filename}")
+    unless File.directory?(uploadername)
+      FileUtils.mkdir_p(uploadername)
+    end
+    FileUtils.mv("#{data.filename}", "#{DOCS_PATH}/#{data_format}/#{data.uploader}/#{data.filename}")
   end
 
   def download_data(url)
-    YoutubeDL.download url
+    options = {
+      format: 'mp4'
+    }
+
+    YoutubeDL.download url, options
   end
 
   def log_data(data)
