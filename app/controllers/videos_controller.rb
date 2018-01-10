ce087c468e2e88bed4402eff1ab0c3daf5d78d1d
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-10 16:54:25 +0800

    輸入單片網址，存下mp4，opus，vtt

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,15 +1,16 @@
 class VideosController < ApplicationController
-  before_action :set_video, only: [:show, :edit, :update, :destroy]
+  before_action :set_video, only: [:show]
 
   # GET /videos
   # GET /videos.json
   def index
-    @videos = Video.all
+    @videos = Video.order(updated_at: :desc)
   end
 
   # GET /videos/1
   # GET /videos/1.json
   def show
+    @video = Video.find(params[:id])
   end
 
   # GET /videos/new
@@ -17,20 +18,14 @@ class VideosController < ApplicationController
     @video = Video.new
   end
 
-  # GET /videos/1/edit
-  # def edit
-  # end
-
-  # POST /videos
-  # POST /videos.json
   def create
     @video = Video.new(video_params)
 
     respond_to do |format|
       if @video.save
         DownloadWorker.perform_async
-
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
+        Video.last.update(status: 'downloading')
+        format.html { redirect_to videos_path, notice: 'Video was successfully created.' }
         format.json { render :show, status: :created, location: @video }
       else
         format.html { render :new }
@@ -39,36 +34,7 @@ class VideosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @video.update(video_params)
-  #       format.html { redirect_to @video, notice: 'Video was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @video }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @video.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /videos/1
-  # DELETE /videos/1.json
-  def destroy
-    @video.destroy
-    respond_to do |format|
-      format.html { redirect_to videos_url, notice: 'Video was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_video
-      @video = Video.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def video_params
       params.require(:video).permit(
@@ -81,7 +47,24 @@ class VideosController < ApplicationController
         :filename,
         :uploader,
         :upload_date,
+        :abr,
+        :acodec,
         :status,
+        :tags,
+        :location,
+        :view_count,
+        :like_count,
+        :dislike_count,
+        :repost_count,
+        :average_rating,
+        :comment_count,
+        :age_limit,
+        :autonumber,
+        :playlist,
+        :playlist_id,
+        :playlist_title,
+        :playlist_uploader,
+        :playlist_uploader_id,
       )
     end
 end
--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -3,41 +3,80 @@ require 'fileutils'
 class DownloadWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
-  DOCS_PATH = File.join(Rails.root, "public")
+  DOCS_PATH = File.join(Rails.root, 'public', 'download')
 
   def perform(*args)
-    # Do something
+    puts 'perform'
     url = Video.last.url
     get_corpus(url)
+    puts '/perform'
   end
 
   def get_corpus(url)
-    data = download_data(url)
-    data_format = data.filename.split('.').last
-    log_data(data)
-
-    dirname = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader}")
-    unless File.directory?(dirname)
-      FileUtils.mkdir_p(dirname)
-    end
-    uploadername = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader}/#{data.filename}")
-    unless File.directory?(uploadername)
-      FileUtils.mkdir_p(uploadername)
-    end
-    FileUtils.mv("#{data.filename}", "#{DOCS_PATH}/#{data_format}/#{data.uploader}/#{data.filename}")
+    puts 'get_corpus'
+    params = {data_formats: ['mp4', 'opus'], url: url}
+    download_data(params)
+    puts 'get_corpus/'
   end
 
-  def download_data(url)
-    options = {
-      format: 'mp4'
-    }
+  def download_data(params={})
+    puts 'download_data'
+    url = params[:url]
+    data_formats = params[:data_formats]
 
-    YoutubeDL.download url, options
+    data_formats.each do |data_format|
+      options = case data_format
+      when 'opus'
+        {
+        'extract-audio': true,
+        'audio-format': data_format,
+        'audio-quality': 0,
+        }
+      when 'mp4'
+        {
+          'write-sub': true,
+          'format': data_format,
+        }
+      end
+      data = YoutubeDL.download url, options
+      params = params.merge(data: data, data_format: data_format)
+      move_files(params)
+      log_data(data)
+    end
   end
+
+  def move_files(params={})
+    data = params[:data]
+    data_format = params[:data_format]
+    move_file(params)
+    move_file({data: data, data_format: 'vtt'}) if data_format == 'mp4'
+
+    Video.last.update(status: 'downloaded')
+  end
+
+ def	move_file(params={})
+   data = params[:data]
+   data_format = params[:data_format]
+
+   data_filename = File.basename data.filename, (File.extname data.filename)
+   dirname = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}")
+   unless File.directory?(dirname)
+     FileUtils.mkdir_p(dirname)
+   end
+
+   uploadername = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data.filename}")
+   unless File.directory?(uploadername)
+     FileUtils.mkdir_p(uploadername)
+   end
+   if data_format == 'vtt'
+     FileUtils.mv("#{data_filename}.zh-TW.vtt", "#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data_filename}.zh-TW.vtt")
+   else
+     FileUtils.mv("#{data_filename}.#{data_format}", "#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data_filename}.#{data_format}")
+   end
+ end
 
   def log_data(data)
-    Video.find_by(url: data.url).update(
-      status: 'downloaded',
+    Video.last.update(
       yid: data.id,
       title: data.title,
       thumbnail: data.thumbnail,
@@ -45,7 +84,25 @@ class DownloadWorker
       duration: data.duration,
       filename: data.filename,
       uploader: data.uploader,
+      uploader_id: data.uploader_id,
       upload_date: data.upload_date,
+      abr: data.abr,
+      acodec: data.acodec,
+      view_count: data.view_count,
+      like_count: data.like_count,
+      dislike_count: data.dislike_count,
+      average_rating: data.average_rating,
+      age_limit: data.age_limit,
     )
+    # Not all video has..
+    # repost_count: data.repost_count,
+    # comment_count: data.comment_count,
+    # location: data.location,
+    # autonumber: data.autonumber,
+    # playlist: data.playlist,
+    # playlist_id: data.playlist_id,
+    # playlist_title: data.playlist_title,
+    # playlist_uploader: data.playlist_uploader,
+    # playlist_uploader_id:	data.playlist_uploader_id,
   end
 end
