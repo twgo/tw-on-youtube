1e9b950c2c09c65da0bfc2837de4c21b635254ad
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-11 13:55:41 +0800

    add mvc spec and block duplicated url input

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'videos#new'
 
-  resources :videos
+  resources :videos, only: %i[index new create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
