f169f1cfda4ad42b4759af3297d1c29e1a4cfae7
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-11 10:30:26 +0800

    修正檔案移動，存在檔案時移動

--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -6,21 +6,16 @@ class DownloadWorker
   DOCS_PATH = File.join(Rails.root, 'public', 'download')
 
   def perform(*args)
-    puts 'perform'
     url = Video.last.url
     get_corpus(url)
-    puts '/perform'
   end
 
   def get_corpus(url)
-    puts 'get_corpus'
     params = {data_formats: ['mp4', 'opus'], url: url}
     download_data(params)
-    puts 'get_corpus/'
   end
 
   def download_data(params={})
-    puts 'download_data'
     url = params[:url]
     data_formats = params[:data_formats]
 
@@ -36,6 +31,7 @@ class DownloadWorker
         {
           'write-sub': true,
           'format': data_format,
+          'sub-lang': 'zh-TW'
         }
       end
       data = YoutubeDL.download url, options
@@ -57,22 +53,19 @@ class DownloadWorker
  def	move_file(params={})
    data = params[:data]
    data_format = params[:data_format]
+   downloaded_files = Dir[File.join("*.#{data_format}")]
 
-   data_filename = File.basename data.filename, (File.extname data.filename)
-   dirname = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}")
-   unless File.directory?(dirname)
-     FileUtils.mkdir_p(dirname)
-   end
-
-   uploadername = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data.filename}")
-   unless File.directory?(uploadername)
-     FileUtils.mkdir_p(uploadername)
-   end
-   if data_format == 'vtt'
-     FileUtils.mv("#{data_filename}.zh-TW.vtt", "#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data_filename}.zh-TW.vtt")
-   else
-     FileUtils.mv("#{data_filename}.#{data_format}", "#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{data_filename}.#{data_format}")
-   end
+   downloaded_files.each do |downloaded_filename|
+     dirname = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}")
+     unless File.directory?(dirname)
+       FileUtils.mkdir_p(dirname)
+     end
+     uploadername = File.dirname("#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{downloaded_filename}")
+     unless File.directory?(uploadername)
+       FileUtils.mkdir_p(uploadername)
+     end
+     FileUtils.mv("#{downloaded_filename}", "#{DOCS_PATH}/#{data_format}/#{data.uploader_id}/#{downloaded_filename}")
+   end if downloaded_files.any?
  end
 
   def log_data(data)
