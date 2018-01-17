11a50e03cc5339d2bf4f8c464155dbf5bfe52f59
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-17 08:09:32 +0800

    remove after

--- spec/helpers/videos_helper_spec.rb
+++ spec/helpers/videos_helper_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe VideosHelper, type: :helper do
-  before do
-    Video.create(url: 'url', status: 'downloading', filename: 'filename')
-  end
-
   it 'return blanks while no files' do
+    Video.create(url: 'url', status: 'downloading', filename: 'filename')
     expect(file_link(Video.last)).to eq '  '
-  end
-
-  after do
-    Video.last.delete
   end
 end
