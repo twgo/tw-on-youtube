1c9cf7dec022a0b8fb65dec4f85ea5e238ecadad
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-18 09:34:29 +0800

    UrlValidator

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,13 @@
+class UrlValidator < ActiveModel::Validator
+  def validate(input)
+    unless ((input.url.exclude? 'channel') && (input.url.exclude? 'playlist') && (input.url.include? 'youtube') && ((input.url.include? 'http') || (input.url.include? 'https')))
+      input.errors[:url] << 'Youtube single video url only please! ex. https://www.youtube.com/watch?v=fhnlIBmVk6I'
+    end
+  end
+end
+
 class Video < ApplicationRecord
   validates_presence_of :url
   validates_uniqueness_of :url
+  validates_with UrlValidator
 end
