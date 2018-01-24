df7b29d65b2e56dead1226145c72ac411b50eb84
Yang-Hsiang Chang <leo424y@gmail.com>
2018-01-24 10:18:14 +0800

    videos_helper spec

--- spec/helpers/videos_helper_spec.rb
+++ spec/helpers/videos_helper_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 include ApplicationHelper
 
 RSpec.describe VideosHelper, type: :helper do
+  before do
+    @url = 'https://www.youtube.com/watch?v=fhnlIBmVk6I'
+  end
   it 'return blanks while no files' do
-    url = 'https://www.youtube.com/watch?v=fhnlIBmVk6I'
-    Video.create(url: url, status: 'downloading', filename: 'filename', format_downloaded: 'mp4')
-    expect(file_link(Video.find_by(url: url))).to eq ''
+    Video.create(url: @url, status: 'downloading', filename: 'filename', format_downloaded: 'mp4', uploader_id: 'test_uid')
+    expect(file_link(Video.find_by(url: @url))).to eq ''
   end
   it 'return vtt' do
-    url = 'https://www.youtube.com/watch?v=fhnlIBmVk6I'
-    Video.create(url: url, status: 'downloading', filename: 'filename', format_downloaded: 'vtt')
-    expect(file_link(Video.find_by(url: url))).to eq ''
+    FileUtils.mkdir_p('public/download/vtt/test_uid')
+    File.open("public/download/vtt/test_uid/test.vtt", 'a') {|f| f.write("test vtt") }
+    Video.create(url: @url, status: 'downloading', filename: 'filename', format_downloaded: 'vtt', uploader_id: 'test_uid')
+    expect(file_link(Video.find_by(url: @url))).to eq "<a target=\"_blank\" href=\"download/vtt/test_uid/test.vtt\">test</a>"
+    FileUtils.rm_rf(Dir.glob('public/download/vtt/test_uid/*'))
+    FileUtils.rm_rf('public/download/vtt/test_uid/')
   end
 end
