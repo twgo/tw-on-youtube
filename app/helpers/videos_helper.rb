a4fc086755a247d5ab1a4abf06f328e788594093
yang-hsiang chang <leo424y@gmail.com>
2018-12-12 17:01:24 +0800

    uploader_id!

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,16 +2,16 @@ module VideosHelper
   def file_link(video)
     data_formats = video.format_downloaded.split(' ')
     links = data_formats.map do |data_format|
-      folder = "download/#{data_format}/#{video.channel_id}"
-      path = File.join(Rails.root, 'public', 'download', "#{data_format}", "#{video.channel_id}")
+      folder = "download/#{data_format}/#{video.uploader_id}"
+      path = File.join(Rails.root, 'public', 'download', "#{data_format}", "#{video.uploader_id}")
       data_name = file_name video.filename
       if data_format == 'vtt'
-        vtts = Dir["public/download/mp4/#{video.channel_id}/*.vtt"]
+        vtts = Dir["public/download/mp4/#{video.uploader_id}/*.vtt"]
         vtts.map do |vtt|
           vtt_name = file_name vtt
           show_vtt = vtt_name.split('.')[-1]
           # link_to(show_vtt, "#{folder}/#{vtt_name}.vtt", target: '_blank')
-          link_to(show_vtt, get_vtt_videos_path(p: vtt_name, u: video.channel_id), target: '_blank')
+          link_to(show_vtt, get_vtt_videos_path(p: vtt_name, u: video.uploader_id), target: '_blank')
         end if vtts.any?
       else
         uploader_dirname = File.file?("#{path}/#{data_name}.#{data_format}")
--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -44,7 +44,7 @@ class DownloadWorker
         'extract-audio': true,
         'audio-format': 'wav',
         'audio-quality': 0,
-        'output': 'public/download/wav/%(channel_id)s/%(channel_id)s%-%(playlist_id)s-%(id)s.%(ext)s',
+        'output': 'public/download/wav/%(uploader_id)s/%(uploader_id)s%-%(playlist_id)s-%(id)s.%(ext)s',
         'download-archive': 'public/download/wav-archive.txt',
       }
     elsif data_format == 'mp4'
@@ -52,7 +52,7 @@ class DownloadWorker
         'write-sub': true,
         'format': 'mp4',
         'sub-lang': 'zh-Hant,zh-Hans,en',
-        'output': 'public/download/mp4/%(channel_id)s/%(channel_id)s%-%(playlist_id)s-%(id)s.%(ext)s',
+        'output': 'public/download/mp4/%(uploader_id)s/%(uploader_id)s%-%(playlist_id)s-%(id)s.%(ext)s',
         'download-archive': 'public/download/mp4-archive.txt',
       }
     else
@@ -132,7 +132,7 @@ class DownloadWorker
   end
 
   def check_subtitle(data)
-    vtts = Dir[File.join("public/download/mp4/#{data.channel_id}/*.vtt")]
+    vtts = Dir[File.join("public/download/mp4/#{data.uploader_id}/*.vtt")]
     url = data.url
     if vtts.any?
       vtts.each do |vtt|
